d71c03936045f069d0f8424423f0b77d7c8917b0
22520570 <22520570@gm.uit.edu.vn>
2024-12-26 19:51:30 +0700

    employee crud api

--- controllers/employee.controllers.js
+++ controllers/employee.controllers.js
@@ -24,6 +24,83 @@ const createEmployee = async (req, res) => {
     }
 };
 
+const getAllEmployee = async (req, res) => {
+    try {
+        const employeeList = await Employee.findAll({
+            where: {
+                status: 1,
+            }
+        });
+        res.status(200).send(employeeList);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+const getDetailEmployee = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const detailEmployee = await Employee.findOne({
+            where: {
+                id: id,
+                status: 1,
+            }
+        });
+        res.status(200).send(detailEmployee);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+const updateEmployee = async (req, res) => {
+    const { id } = req.params;
+    const {
+        name,
+        address,
+        phone,
+        email,
+        birthday,
+    } = req.body;
+    try {
+        const detailEmployee = await Employee.findOne({
+            where: {
+                id: id,
+                status: 1,
+            }
+        });
+        detailEmployee.name = name;
+        detailEmployee.address = address;
+        detailEmployee.phone = phone;
+        detailEmployee.email = email;
+        detailEmployee.birthday = birthday;
+        await detailEmployee.save();
+        res.status(200).send(detailEmployee);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+const deleteEmployee = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const detailEmployee = await Employee.findOne({
+            where: {
+                id: id,
+                status: 1,
+            }
+        });
+        detailEmployee.status = 0;
+        await detailEmployee.save();
+        res.status(200).send(detailEmployee);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
 module.exports = {
     createEmployee,
+    getAllEmployee,
+    getDetailEmployee,
+    updateEmployee,
+    deleteEmployee,
 }

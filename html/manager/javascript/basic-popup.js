7a795cb50b7ca32c457f811ec26a3ef7314afb48
haoken35 <22520406@gm.uit.edu.vn>
2024-12-26 20:10:30 +0700

    popup

---
+++ html/manager/javascript/basic-popup.js
@@ -0,0 +1,14 @@
+const filterButton = document.getElementById('filterButton');
+const filterPopup = document.getElementById('filterPopup');
+
+filterButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    filterPopup.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+    if (!filterPopup.contains(e.target)) {
+        filterPopup.classList.remove('active');
+    }
+});
+

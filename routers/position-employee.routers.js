3e18c036bed925f6b4d6948f0a63f2a7f1cb1490
22520570 <22520570@gm.uit.edu.vn>
2024-12-28 19:31:22 +0700

    fix position employee controllers and router

--- routers/position-employee.routers.js
+++ routers/position-employee.routers.js
@@ -1,90 +1,14 @@
-const { PositionEmployee } = require('../models');
+const express = require('express');
+const { createPositionEmployee, getAllPositionEmployee, getDetailPositionEmployee, updatePositionEmployee, deletePositionEmployee } = require('../controllers/position-employee.controllers');
 
-const createPositionEmployee = async (req, res) => {
-    const {
-        name_position,
-        status
-    } = req.body;
-    try {
-        const newPositionEmployee = await PositionEmployee.create({
-            name_position,
-            status
-        });
-        res.status(201).send(newPositionEmployee);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-};
+const positionEmployeeRouter = express.Router();
 
-const getAllPositionEmployee = async (req, res) => {
-    try {
-        const PositionEmployeeList = await PositionEmployee.findAll({
-            where: {
-                status: 1,
-            }
-        });
-        res.status(200).send(PositionEmployeeList);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-}
-
-const getDetailPositionEmployee = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const detailPositionEmployee = await PositionEmployee.findOne({
-            where: {
-                id: id,
-                status: 1,
-            }
-        });
-        res.status(200).send(detailPositionEmployee);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-}
-
-const updatePositionEmployee = async (req, res) => {
-    const { id } = req.params;
-    const {
-        name_position
-    } = req.body;
-    try {
-        const detailPositionEmployee = await PositionEmployee.findOne({
-            where: {
-                id: id,
-                status: 1,
-            }
-        });
-        detailPositionEmployee.name_position = name_position;
-        await detailPositionEmployee.save();
-        res.status(200).send(detailPositionEmployee);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-}
-
-const deletePositionEmployee = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const detailPositionEmployee = await PositionEmployee.findOne({
-            where: {
-                id: id,
-                status: 1,
-            }
-        });
-        detailPositionEmployee.status = 0;
-        await detailPositionEmployee.save();
-        res.status(200).send(detailPositionEmployee);
-    } catch (error) {
-        res.status(500).send(error)
-    }
-}
+positionEmployeeRouter.post("/", createPositionEmployee);
+positionEmployeeRouter.get("/", getAllPositionEmployee);
+positionEmployeeRouter.get("/:id", getDetailPositionEmployee);
+positionEmployeeRouter.put("/:id", updatePositionEmployee);
+positionEmployeeRouter.delete("/:id", deletePositionEmployee);
 
 module.exports = {
-    createPositionEmployee,
-    getAllPositionEmployee,
-    getDetailPositionEmployee,
-    updatePositionEmployee,
-    deletePositionEmployee,
+    positionEmployeeRouter,
 }

944481a1e4815f2b8d60b83a55b60e8520bf5be0
NotASleeper <22520596@gm.uit.edu.vn>
2024-12-28 18:03:24 +0700

    update index router

--- routers/index.js
+++ routers/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const { customerRouter } = require('./customer.routers');
-const { employeeRouter } = require('./employee.routers');
-const { productRouter } = require('./product.routers');
+const express = require("express");
+const { customerRouter } = require("./customer.routers");
+const { employeeRouter } = require("./employee.routers");
+const { productRouter } = require("./product.routers");
+const { productCategoryRouter } = require("./product-category.routers");
 
 const rootRouter = express.Router();
 
 rootRouter.use("/customers", customerRouter);
 rootRouter.use("/employees", employeeRouter);
 rootRouter.use("/products", productRouter);
+rootRouter.use("/product-categories", productCategoryRouter);
 
 module.exports = {
-    rootRouter,
-}
+  rootRouter,
+};

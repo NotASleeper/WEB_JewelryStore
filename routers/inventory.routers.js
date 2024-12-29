5032edcd218a9c5f9668c14ac13655d02cb89c46
22520570 <22520570@gm.uit.edu.vn>
2024-12-29 17:20:52 +0700

    change product and inventory api

--- routers/inventory.routers.js
+++ routers/inventory.routers.js
@@ -1,12 +1,9 @@
-//Chắc không cần dùng cái này
-//Gộp chung bên product rồi
-
 const express = require('express');
-const { createInventory, getAllInventory, getDetailInventory, updateInventory } = require('../controllers/inventory.controllers');
+const { getAllInventory, getDetailInventory, updateInventory } = require('../controllers/inventory.controllers');
 
 const inventoryRouter = express.Router();
 
-inventoryRouter.post("/", createInventory);
+// inventoryRouter.post("/", createInventory);
 inventoryRouter.get("/", getAllInventory);
 inventoryRouter.get("/:id", getDetailInventory);
 inventoryRouter.put("/:id", updateInventory);

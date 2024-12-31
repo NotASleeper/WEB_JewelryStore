85d0b0c57fb2a0ec363a59fc50928d63539373e6
22520570 <22520570@gm.uit.edu.vn>
2024-12-31 21:23:41 +0700

    update search url

--- html/manager/js/employee.js
+++ html/manager/js/employee.js
@@ -79,14 +79,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.getElementById('search').addEventListener('click', () => {
     const input = document.getElementById('employeeName').value;
-    const url = `http://localhost:5501/admin/employee.html?name=${encodeURIComponent(input)}`;
-    window.location.href = url;
+    if (input) {
+        const url = `http://localhost:5501/admin/employee.html?name=${encodeURIComponent(input)}`;
+        window.location.href = url;
+    } else {
+        const url = `http://localhost:5501/admin/employee.html`;
+        window.location.href = url;
+    }
 });
 
 document.getElementById('employeeName').addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         const input = document.getElementById('employeeName').value;
-        const url = `http://localhost:5501/admin/employee.html?name=${encodeURIComponent(input)}`;
-        window.location.href = url;
+        if (input) {
+            const url = `http://localhost:5501/admin/employee.html?name=${encodeURIComponent(input)}`;
+            window.location.href = url;
+        } else {
+            const url = `http://localhost:5501/admin/employee.html`;
+            window.location.href = url;
+        }
     }
 });
--- html/manager/js/import.js
+++ html/manager/js/import.js
@@ -65,14 +65,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.getElementById('search').addEventListener('click', () => {
     const input = document.getElementById('supplierName').value;
-    const url = `http://localhost:5501/admin/import.html?supplier=${encodeURIComponent(input)}`;
-    window.location.href = url;
+    if (input) {
+        const url = `http://localhost:5501/admin/import.html?supplier=${encodeURIComponent(input)}`;
+        window.location.href = url;
+    } else {
+        const url = `http://localhost:5501/admin/import.html`;
+        window.location.href = url;
+    }
 });
 
 document.getElementById('supplierName').addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         const input = document.getElementById('supplierName').value;
-        const url = `http://localhost:5501/admin/import.html?supplier=${encodeURIComponent(input)}`;
-        window.location.href = url;
+        if (input) {
+            const url = `http://localhost:5501/admin/import.html?supplier=${encodeURIComponent(input)}`;
+            window.location.href = url;
+        } else {
+            const url = `http://localhost:5501/admin/import.html`;
+            window.location.href = url;
+        }
     }
 });

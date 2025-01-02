65c1ee6f9c1f15457fa3cf2537a1ba77286820a6
22520570 <22520570@gm.uit.edu.vn>
2025-01-02 20:34:13 +0700

    fetch api create product and gemstone

--- routers/gemstone.routers.js
+++ routers/gemstone.routers.js
@@ -1,11 +1,11 @@
 const { Gemstone } = require('../models')
 const express = require('express');
-const { getAllGemstone, getDetailGemstone, updateGemstone, deleteGemstone } = require('../controllers/gemstone.controllers');
+const { getAllGemstone, getDetailGemstone, updateGemstone, deleteGemstone, createGemstone } = require('../controllers/gemstone.controllers');
 const { checkExist } = require('../middlewares/validations/checkExist');
 
 const gemstoneRouter = express.Router();
 
-// gemstoneRouter.post("/", createGemstone);
+gemstoneRouter.post("/", createGemstone);
 gemstoneRouter.get("/", getAllGemstone);
 gemstoneRouter.get("/:id", checkExist(Gemstone), getDetailGemstone);
 gemstoneRouter.put("/:id", checkExist(Gemstone), updateGemstone);
